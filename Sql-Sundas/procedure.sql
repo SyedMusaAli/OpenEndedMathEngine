f3a065311f5c023415c4dbef5550397412a549bb
SyedMusaAli <syed.musa.ali@outlook.com>
2013-09-24 18:11:07 +0500

    Update procedure.sql
    still has issues. the column trick not allowed

--- Sql-Sundas/procedure.sql
+++ Sql-Sundas/procedure.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE PROCEDURE undo(tillDate IN DATE) IS
+CREATE OR REPLACE PROCEDURE undoStep(tillDate IN DATE) IS
 
-cursor c_tab = Select StudId, SemesterID, CourseID, column_name, old_value into  from stagingTable where changeTime >= tillDate;
+cursor c_tab is Select StudId, SemesterID, CourseID, columnName, old_value from stagingTable where changeTime >= tillDate;
 
 BEGIN
 
@@ -8,8 +8,8 @@ BEGIN
 for v_c in c_tab loop
 
 update CourseAllocation
-	set :v_c.column_name = :v_c.old_value
-	where StudId = :v_c.StudId and SemesterID = :v_c.SemesterID and CourseID = :v_c.CourseID;
+set v_c.columnName = v_c.old_value
+where StudId = v_c.StudId and SemesterID = v_c.SemesterID and CourseID = v_c.CourseID;
 
 end loop;
 
